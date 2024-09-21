7923d5becab8eb5295e8d74dd083788d9e5ebbc5
Terrificlev <167452488+Terrificlev@users.noreply.github.com>
2024-09-21 13:13:33 +0200

    Now the Toad jumps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,50 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1.0F;
+    public float tileSize = 50f;
+    public float jumpTime = 2f;
+
+    private float remainingTime = 0;
+    private Vector3 direction; 
+    
     void Update()
     {
-	    var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-	    transform.position += move * speed * Time.deltaTime;
+        if (remainingTime <= 0)
+        {
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                remainingTime = jumpTime;
+                direction = Vector3.up;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                remainingTime = jumpTime;
+                direction = Vector3.left;
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                remainingTime = jumpTime;
+                direction = Vector3.right;
+            }
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                remainingTime = jumpTime;
+                direction = Vector3.down;
+            }
+
+
+            return;
+        }
+        
+        remainingTime -= Time.deltaTime;
+        transform.position += direction * Time.deltaTime * (tileSize / jumpTime);
+
+
     }
 }
