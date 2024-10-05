71fc69a4dec247955f3cf9a26ee8865d4a409855
Terrificlev <167452488+Terrificlev@users.noreply.github.com>
2024-10-05 13:11:36 +0200

    Now there are more lanes for cars, also now cars start on the road

--- Assets/Scripts/CarManager.cs
+++ Assets/Scripts/CarManager.cs
@@ -4,18 +4,34 @@ using UnityEngine.Serialization;
 
 public class CarManager : MonoBehaviour
 {
+    //public CarMovementrighttoleft carPrefab;
     public GameObject carPrefab;
     private Unity.Mathematics.Random _randomGenerator = new Unity.Mathematics.Random();
     
     private float randomHold = -1;
-    public float x = 0;
-    public float y = 0;
+    public float startX = 11f;
+
+    public float minDistance = 1.5f;
+    public float maxDistance = 3f;
+
+    public float minWaitTime = 0.25f;
+    public float maxWaitTime = 1f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        var newX = startX;
+        var newCar1 = Instantiate(carPrefab, transform, true);
+        newCar1.transform.localPosition = new Vector3(newX, 0, -0.1f);
         _randomGenerator.InitState();
-        Instantiate(carPrefab,new Vector3(x,y,-0.1f), new Quaternion(0,0,0,0), transform);
+        while (newX > -11)
+        {
+            var randomDistance = _randomGenerator.NextFloat(minDistance, maxDistance);
+            var newCar2 = Instantiate(carPrefab, transform, true);
+            newCar2.transform.localPosition = new Vector3(newX - randomDistance, 0, -0.1f);
+            newX -= randomDistance;
+        }
+
         
     }
     // Update is called once per frame
@@ -23,7 +39,7 @@ public class CarManager : MonoBehaviour
     {
         while (randomHold <= 0)
         {
-            var random = _randomGenerator.NextFloat(1f, 3.5f);
+            var random = _randomGenerator.NextFloat(minWaitTime, maxWaitTime);
             randomHold = random;
         }
 
@@ -31,8 +47,8 @@ public class CarManager : MonoBehaviour
 
         if (randomHold <= 0)
         {
-            Instantiate(carPrefab,new Vector3(x,y,-0.1f), new Quaternion(0,0,0,0), transform);
-            
+        var newCar1 = Instantiate(carPrefab, transform, true);
+        newCar1.transform.localPosition = new Vector3(startX, 0, -0.1f);
         }
 
     }
